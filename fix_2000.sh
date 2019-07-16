fa473816ceed3676819fef0715c15d0473bd541a
Ethan <39338996+CodeCrazy97@users.noreply.github.com>
2019-07-16 16:30:09 -0400

    Fetching state names

--- fix_2000.sh
+++ fix_2000.sh
@@ -1,11 +1,26 @@
-##############################################################
-# This shell script fixes the 2000 election results, 
-# which essentially caused every space to create a newline.
-##############################################################
+################################################################################################
+# This shell script fixes the 2000 election results, as seen in the 2000.error.txt file.
+# Essentially, the results for each state (popular votes and electoral votes) are on new lines.
+################################################################################################
+ERROR2000="C:\\Users\\A521646\\OneDrive - Valvoline\\Documents\\Projects\\Presidential Election\\2000error.txt"
+FIXEDFILE="C:\\Users\\A521646\\OneDrive - Valvoline\\Documents\\Projects\\Presidential Election\\2000.txt"
+rm "$FIXEDFILE"  # Clear out anything that was there from a previous run of the script.
+touch "$FIXEDFILE"
 
-while read p; do
-	# Check to see if the line is the name of a state.
-	if [[ $p =~ ^[A-Za-z_] ]]; then
-	  echo "$p"
-	fi
-done <2000error.txt
+state=""
+while read p; do	
+	echo $p | awk -v ORS="" '{ gsub(/./,"&\n") ; print }' | \
+	while read char
+	do
+		if [[ "$char" == [A-Z] ]]; then  # The character is alphabetic. It is therefore part of a state name.
+			state=${state}$char			
+			if [[ ${#state} == 2 ]]; then  # We have got the entire state name if it's length is exactly 2 chars.
+				echo "STATE: $state" 	
+				cat "\r\n$state " >> $FIXEDFILE
+			fi			
+		else
+			state=""
+		fi 
+	done
+	echo $state
+done <"$ERROR2000"
