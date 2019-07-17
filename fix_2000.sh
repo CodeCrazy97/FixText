e086a960f3ca559acfcf09c6a7b201969efa4b62
Ethan <39338996+CodeCrazy97@users.noreply.github.com>
2019-07-17 13:03:55 -0400

    Script now adds states and results to fixed file

--- fix_2000.sh
+++ fix_2000.sh
@@ -13,12 +13,21 @@ while read p; do
 	while read char
 	do
 		if [[ "$char" == [A-Z] ]]; then  # The character is alphabetic. It is therefore part of a state name.
+			if [[ ${#state} == 0 ]]; then   # This is the first character in the state name. Therefore, place the state on a new line.
+				echo "" >> $FIXEDFILE
+			fi
+			
 			state=${state}$char			
 			if [[ ${#state} == 2 ]]; then  # We have got the entire state name if it's length is exactly 2 chars.
-				echo "${state:0:2} " >> $FIXEDFILE  # Send the state name to the file. Trim the last character (a '\n')
-			fi			
+				printf "%s" "${state:0:2} " >> $FIXEDFILE  # Send the state name to the file. Trim the last character (a '\n')								
+			fi
 		else
-			state=""
+			state=""  # Reset state name for next state.							
+			if [[ "$char" != [#] ]]; then
+				printf "%s" "$char" >> $FIXEDFILE
+			else 
+				printf " " >> $FIXEDFILE
+			fi			
 		fi 
 	done
 	echo $state
